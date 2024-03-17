c0228e074e183db2dbb0a47ead782f47c76f789e
Danny Cao <dannycao1997@yahoo.com>
2024-03-16 20:33:03 -0400

    created structure of the game, need to finish methods

--- Hammurabi.java
+++ Hammurabi.java
@@ -16,104 +16,101 @@ Land is currently worth 19 bushels per acre.
 */
 
 
-
 public class Hammurabi {
-    Random rand = new Random();  // instance field variable
+    Random rand = new Random();  // instance field variables
     Scanner scanner = new Scanner(System.in);
-
+    private int population = 100;
+    private int grainInStorage = 2800;
+    private int acresOwned = 1000;
+    private int landValue = 19;
 
     public static void main(String[] args) { // required in every Java program
         new Hammurabi().playGame();
     }
 
-
     void playGame() {
-        // declare local variables here: grain, population, etc.
-        // statements go after the declarations
-
-
-        //initializing conditions for starting game
-        int year = 1;
-        int population = 100;
-        int grainInStorage = 2800;
-        int acresOwned = 1000;
-        int landValue = 19;
-
-
+        // need a SOUT string statement here >>>>>
+        // ROUGH DRAFT BACK BONE
         //starting game prompt: creating game loop for each of the 10 years
-        for(int year = 1; year <= 10; year++) {
+        for (int year = 1; year <= 10; year++) {
+            printSummary(year);
+            int acresToBuy = askHowManyAcresToBuy(landValue, grainInStorage);
+            if (acresToBuy > 0) {
+                acresOwned += acresToBuy;
+                grainInStorage -= acresToBuy * landValue;
+            } else {
+                int acresToSell = askHowManyAcresToSell(acresOwned);
+                acresOwned -= acresToSell;
+            }
+
+            int grainToFeed = askHowMuchGrainToFeedPeople(grainInStorage);
+            grainInStorage -= grainToFeed;
+
+            int acresToPlant = askHowManyAcresToPlant(acresOwned, population, grainInStorage);
+            grainInStorage -= acresToPlant * 2; // 2 buhles per acre to plant
+
+            // following year
+
+            int plagueDeaths = plagueDeaths(population);
+            population -= plagueDeaths;
+
+            int starvationDeaths = starvationDeaths(population, grainToFeed);
+            population -= starvationDeaths;
+            if(uprising(population, starvationDeaths)) {
+                System.out.println("YOU'VE BEEN KICKED OUT OF OFFICE!");
+                break;
+            }
+
+            int immigrants = immigrants(population, acresOwned, grainInStorage);
+            population += immigrants;
+
+            // rough draft update 
 
 
-            // ROUGH DRAFT BACK BONE
 
-        }// methods
+
+
+
+
         int askHowManyAcresToBuy(int price, int bushels){
-
         }
 
         int askHowManyAcresToSell(int acresOwned){
-
         }
 
         int askHowMuchGrainToFeedPeople(int bushels){
-
         }
 
         int askHowManyAcresToPlant(int acresOwned, int population, int bushels){
-
         }
 
         int plagueDeaths(int population){
-
         }
 
         int starvationDeaths(int population, int bushelsFedToPeople){
-
         }
 
         boolean uprising(int population, int howManyPeopleStarved){
-
         }
 
         int immigrants(int population, int acresOwned, int grainInStorage){
-
         }
 
         int harvest(int acres, int bushelsUsedAsSeed){
-
         }
 
         int grainEatenByRats(int bushels){
-
         }
 
-        int newCostOfLand(){
-
+        int newCostOfLand() {
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-    }
-
-    //Saving this code for later
-            /*System.out.println("0 great Hammurabi!");
+        void printSummary(int year){
+            System.out.println("0 great Hammurabi!");
             System.out.println("You are in year " + year + " of your ten year rule.");
             System.out.println("In the previous year " + year + " people starved to death.");
             System.out.println("The population is now " + population + ".");
-            System.out.println("Land is currenty worth " + landValue + " bushels per acre.");*/
-
+            System.out.println("Land is currenty worth " + landValue + " bushels per acre.");
+        }
+    }
